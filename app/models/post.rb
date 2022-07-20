111ce9f55c6c30e9831348b1e24ac454b7ffc0f0
John Garbero <john.garbero.itb@gmail.com>
2022-07-20 08:49:15 -0500

    commit tree 83b463d77bcb612decf04d7fe79defde1cf6912a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :click_bait
+
+    CLICKBAIT_PATTERNS = [
+        "Won't Believe",
+        "Secret",
+        "Top[number]",
+        "Guess"
+    ]
+
+    def click_bait
+        unless title.blank? || CLICKBAIT_PATTERNS.any? {|p| title.match(p)}
+            errors.add(:title, "title not clickbait-y enough")
+        end
+    end
 end
